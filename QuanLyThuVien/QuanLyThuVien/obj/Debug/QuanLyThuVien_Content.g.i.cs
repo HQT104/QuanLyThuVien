7c17379784f467903f0d2156b9bfeeed550c6215
HQT104 <19520289@gm.uit.edu.vn>
2021-12-02 09:40:23 +0700

    update

--- QuanLyThuVien/QuanLyThuVien/obj/Debug/QuanLyThuVien_Content.g.i.cs
+++ QuanLyThuVien/QuanLyThuVien/obj/Debug/QuanLyThuVien_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/s%c3%a1ch.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/picture1.png")]
 
 
